07c1e95ab27053ff77d266e12a574e9aa3d113be
NicowaxDelux <nycko111995@gmail.com>
2024-07-26 21:52:16 -0500

    use function filter

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,9 @@
+export default function getStudentsByLocation(students, city) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const filterCity = students.filter((item) => item.location === city)
+
+  return filterCity;
+}
