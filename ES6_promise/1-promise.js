1a1abf4fc98e4ca45ca79a42812aac3d98f47395
NicowaxDelux <nycko111995@gmail.com>
2024-07-28 16:16:10 -0500

    return a promise with resolve and reject

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getFullResponseFromAPI(success) {
+  const response = new Promise((resolve, reject) => {
+    if (success !== false) {
+        resolve({
+            status: 200,
+            body: 'Success',
+        });
+    }
+    reject (new Error('The fake API is not working currently'));
+  });
+
+  return response;
+}
