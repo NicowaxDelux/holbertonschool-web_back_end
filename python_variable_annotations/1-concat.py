49680cbaf8b15dcf98ba58f22a793f6db0167a58
NicowaxDelux <nycko111995@gmail.com>
2024-08-14 22:07:51 -0500

    Basic annotations - concat

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -6,4 +6,12 @@ and a string str2 as arguments and returns a concatenated string
 
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Prameters:
+    str1: str
+    str2: str
+
+    Returns:
+    str: str1 + str2
+    """
     return str1 + str2
