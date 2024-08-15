0037684a2bb8356fa4661b55c8a237b36ef76ec0
NicowaxDelux <nycko111995@gmail.com>
2024-08-14 21:51:12 -0500

    Basic annotations - add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 def add(a: float, b: float) -> float:
+    """
+    Parameters:
+    a: float
+    b: float
+
+    Returns:
+    float: sum of a an b
+    """
     return a + b
