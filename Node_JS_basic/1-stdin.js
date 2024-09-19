3cf44d9222b395ed638c9693d9370b63f4e6e0ef
NicowaxDelux <nycko111995@gmail.com>
2024-09-18 20:38:11 -0500

    Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,6 +4,9 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.trim()}\r`);
-  console.log('This important software is now closing');
   process.exit(0);
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
