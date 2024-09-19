5da211faf0f5a48081a28b6f53237ff52339d90c
NicowaxDelux <nycko111995@gmail.com>
2024-09-18 20:21:41 -0500

    Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,11 +3,7 @@ process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.trim()}\r`);
+  console.log(`Your name is: ${data.trim()}`);
   console.log('This important software is now closing');
   process.exit(0);
-});
-
-process.stdin.on('error', (err) => {
-  console.error('An error occurred: ', err);
 });
