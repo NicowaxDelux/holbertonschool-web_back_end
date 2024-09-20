a23f6a553b58217af5d93d84916005e11805456f
NicowaxDelux <nycko111995@gmail.com>
2024-09-20 14:43:42 -0500

    Reading a file asynchronously with Node JS

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,38 @@
+const fs = require('fs').promises;
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      const lines = data.trim().split('\n');
+
+      if (lines.length <= 1) {
+        throw new Error('Cannot load the database');
+      }
+      const students = lines.slice(1).filter((line) => line).map((line) => {
+        const [firstname, , , field] = line.split(',');
+        return { firstname, field };
+      });
+
+      console.log(`Number of students: ${students.length}`);
+
+      const fields = {};
+
+      students.forEach(({ firstname, field }) => {
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      });
+
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        }
+      }
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
+}
+
+module.exports = countStudents;
