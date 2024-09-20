e3b98cc733d9930e1f83ab8fa07916ccb92b66bd
NicowaxDelux <nycko111995@gmail.com>
2024-09-20 16:39:51 -0500

    Create a small HTTP server using Node's HTTP module

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,6 +1,9 @@
 const app = require('http');
 
 app.createServer((req, res) => {
-  res.write('Hello Holberton School!');
-  res.end();
-}).listen(1245);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+}).listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
