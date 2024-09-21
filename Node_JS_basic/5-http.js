b02e5d215d5af9d7f7fe58367da7dd48cd52eb91
NicowaxDelux <nycko111995@gmail.com>
2024-09-20 20:40:07 -0500

    Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,34 +4,35 @@ const countStudents = require('./3-read_file_async');
 const database = process.argv[2];
 
 const app = http.createServer((req, res) => {
-  const reqUrl = url.parse(req.url, true).pathname;
+    const reqURL = url.parse(req.url, true).pathname;
 
-  if (reqUrl === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!');
-  } else if (reqUrl === '/students') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    
-    res.write('This is the list of our students\n');
-    
-    countStudents(database)
-      .then((data) => {
-        res.end(data);
-      })
-      .catch((err) => {
-        res.statusCode = 500;
-        res.end(err.message);
-      });
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found');
-  }
+    if (reqURL === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello Holberton School!');
+    }
+    if (reqURL === '/students') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.write('This is the list of our students\n');
+
+        countStudents(database)
+            .then((data) => {
+                res.end(data);
+            })
+            .catch((err) => {
+                res.statusCode = 500;
+                res.end(err.message);
+            });
+    }
+    if (reqURL !== '/' && reqURL !== '/students') {
+        res.statusCode = 404;
+        res.end('No Found');
+    }
 });
 
 app.listen(1245, () => {
-  console.log('Server is running on port 1245');
+    console.log('Server is running on port 1245')
 });
 
 module.exports = app;
