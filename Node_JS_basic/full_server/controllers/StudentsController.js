8aed3f1987f97f7eb27a55471aef8fe31edfa4c9
NicowaxDelux <nycko111995@gmail.com>
2024-09-22 00:58:44 -0500

    8. Organize a complex HTTP server using Express

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,41 +1,41 @@
-import { readDatabase } from '../utils';
+import readDatabase from '../utils';
 
 class StudentsController {
-    static async getAllStudents(req, res) {
-        try {
-            const students = await readDatabase('./database.csv');
-            const response = ['This is the list of our students'];
+  static async getAllStudents(req, res) {
+    try {
+      const students = await readDatabase('./database.csv');
+      const response = ['This is the list of our students'];
 
-            for (const field in students) {
-                response.push(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
-            }
-
-            res.status(200).send(response.join('\n'));
-        } catch (error) {
-            res.status(500).send('Cannot load the database');
+      for (const field in students) {
+        if (Object.prototype.hasOwnProperty.call(students, field)) {
+          response.push(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
         }
+      }
+
+      res.status(200).send(response.join('\n'));
+    } catch (error) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+
+  static async getAllStudentsByMajor({ params: { major } }, res) {
+    if (major !== 'CS' && major !== 'SWE') {
+      return res.status(500).send('Major parameter must be CS or SWE');
     }
 
-    static async getAllStudentsByMajor(req, res) {
-        const major = req.params.major;
+    try {
+      const students = await readDatabase('./database.csv');
+      const list = students[major] || [];
 
-        if (major !== 'CS' && major !== 'SWE') {
-            return res.status(500).send('Major parameter must be CS or SWE');
-        }
+      if (list.length === 0) {
+        return res.status(200).send(`List: No students found for ${major}`);
+      }
 
-        try {
-            const students = await readDatabase('./database.csv');
-            const list = students[major] || [];
-
-            if (list.length === 0) {
-                return res.status(200).send(`List: No students found for ${major}`);
-            }
-
-            res.status(200).send(`List: ${list.join(', ')}`);
-        } catch (error) {
-            res.status(500).send('Cannot load the database');
-        }
+      return res.status(200).send(`List: ${list.join(', ')}`); // Retorno aquí también
+    } catch (error) {
+      return res.status(500).send('Cannot load the database'); // Agregando retorno aquí
     }
+  }
 }
 
 export default StudentsController;
